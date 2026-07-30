bbcfa596fa3a015d3b2196653bda91c5e15f526d
Sen Nary <sennary@Sens-MacBook-Pro.local>
2026-07-30 11:31:13 +0700

    Fix vite config base path for GitHub Pages subpath deployment

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,12 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite' 
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss()
-  ],
-  base: "./",
+export default defineConfig(({ command }) => {
+  return {
+    plugins: [
+      react(),
+      tailwindcss()
+    ],
+    base: command === 'build' ? '/my-post-app/' : '/',
+  }
 })
